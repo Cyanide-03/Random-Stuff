21f3cf54766c38b7910f7bb0a061fccda2d78c5d
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-18 13:18:31 +0000

    add initial implementation of falling ball simulation with OpenGL

---
+++ Falling_ball.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+float ballY=0.5f;
+float ballSpeed=0.0f;
+float acc=-9.8f;
+float dt=0.016f;
+
+void initopenGL(){
+    if(!glfwInit()){
+        std::cerr<<"Error initializing glfw";
+        exit(1);
+    }
+    GLFWwindow *window=glfwCreateWindow(800,600,"Falling Ball",NULL,NULL);
+    if(!window){
+        std::cerr<<"Error creating window";
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+    glewInit();
+}
+
