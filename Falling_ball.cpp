b5dbcf8d06f9a4d39933ae6eb8be7cb99d416a9f
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-18 14:23:32 +0000

    add circle rendering and rectangle rendering functions to depict two shapes bouncing

--- Falling_ball.cpp
+++ Falling_ball.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 float ballY=0.5f;
 float ballSpeed=0.0f;
@@ -32,13 +33,30 @@ void update(){
     }
 }
 
-void render(){
-    glClear(GL_COLOR_BUFFER_BIT);
+void rect_render(){
     glBegin(GL_QUADS);
-    glVertex2f(-0.1f,ballY-0.1f);
-    glVertex2f(0.1f,ballY-0.1f);
-    glVertex2f(0.1f,ballY+0.1f);
-    glVertex2f(-0.1f,ballY+0.1f);
+    glColor3f(0.0f,1.0f,0.0f);
+    glVertex2f(-0.3f,ballY-0.1f);
+    glVertex2f(-0.5f,ballY-0.1f);
+    glVertex2f(-0.5f,ballY+0.1f);
+    glVertex2f(-0.3f,ballY+0.1f);
+    glEnd();
+}
+
+void circle_render(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0f,1.0f,0.0f);
+    glBegin(GL_TRIANGLE_FAN);
+
+    glVertex2f(0.4f, ballY);
+    float radius=0.1f;
+    for(int i=0;i<=100;i++){
+        float angle=2*M_PI*i/100;
+        float x=radius*cos(angle);
+        float y=radius*sin(angle);
+        glVertex2f(0.4f+x,ballY+y);
+    }
+
     glEnd();
 }
 
@@ -48,7 +66,8 @@ int main(){
 
     while(!glfwWindowShouldClose(window)){
         update();
-        render();
+        circle_render();
+        rect_render();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
