5a0b8a05045152247c2b90c344517ad0fdd8fd8d
Cyanide-03 <aditya.ag0301@gmail.com>
2025-10-12 17:15:24 +0000

    Add initial implementation of PageRank algorithm with transition model

---
+++ AI/pagerank/pagerank.py
@@ -0,0 +1,105 @@
+import os
+import random
+import re
+import sys
+
+DAMPING = 0.85
+SAMPLES = 10000
+
+def crawl(directory):
+    """
+    Parse a directory of HTML pages and check for links to other pages.
+    Return a dictionary where each key is a page, and values are
+    a list of all other pages in the corpus that are linked to by the page.
+    """
+    pages = dict()
+
+    # Extract all links from HTML files
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
+            pages[filename] = set(links) - {filename}
+
+    # Only include links to other pages in the corpus
+    for filename in pages:
+        pages[filename] = set(
+            link for link in pages[filename]
+            if link in pages
+        )
+
+    return pages
+
+
+def transition_model(corpus, curr_page, damping_factor=DAMPING):
+    """
+    Return a probability distribution over which page to visit next,
+    given a current page.
+
+    With probability `damping_factor`, choose a link at random
+    linked to by `page`. With probability `1 - damping_factor`, choose
+    a link at random chosen from all pages in the corpus.
+    """
+    curr_links=[link for link in corpus[curr_page]]
+    total_pages=len(corpus)
+    num_links=len(curr_links)
+
+    if num_links==0:
+        probs={page:1/total_pages for page in corpus}
+
+    else:
+        undamped_prob=(1-damping_factor)/total_pages
+        probs={page:undamped_prob for page in corpus}
+
+        damped_prob=damping_factor/num_links 
+        for link in curr_links:
+            probs[link]+=damped_prob
+    
+    return probs
+    # raise NotImplementedError
+
+
+def sample_pagerank(corpus, damping_factor=DAMPING, n=SAMPLES):
+    """
+    Return PageRank values for each page by sampling `n` pages
+    according to transition model, starting with a page at random.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    # start from a random page and then using transition model get the probs 
+    # and then using those sample another page 
+    raise NotImplementedError
+
+
+def iterate_pagerank(corpus, damping_factor=DAMPING):
+    """
+    Return PageRank values for each page by iteratively updating
+    PageRank values until convergence.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    raise NotImplementedError
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+
+
+if __name__ == "__main__":
+    main()
